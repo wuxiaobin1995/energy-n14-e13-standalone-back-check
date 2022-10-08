3232db670d20b3996849f33a5341bee0fbed13b3
wuxiaobin1995 <845225263@qq.com>
2022-10-08 10:40:19 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 /*
  * @Author      : Mr.bin
  * @Date        : 2022-09-15 09:49:32
- * @LastEditTime: 2022-09-15 09:49:38
+ * @LastEditTime: 2022-10-08 10:23:42
  * @Description : vue.config.js
  */
+
 const projectName = 'energy-n14-e13-standalone-back-check' // 项目名称，是一个程序的唯一标识符
 const softwareName = '全身等长肌力检测软件-单机版' // 软件名称
 
@@ -59,8 +60,8 @@ module.exports = {
         /* nsis安装界面配置 */
         nsis: {
           oneClick: false, // 是否一键安装
-          allowElevation: false, // 是否允许请求提升。如果为false，则用户必须使用提升的权限重新启动安装程序
-          allowToChangeInstallationDirectory: false, // 允许修改安装目录
+          allowElevation: true, // 是否允许请求提升。如果为false，则用户必须使用提升的权限重新启动安装程序
+          allowToChangeInstallationDirectory: true, // 允许修改安装目录
           installerIcon: './public/ico/install/common.ico', // 安装图标
           uninstallerIcon: './public/ico/delete/common.ico', // 卸载图标
           createDesktopShortcut: true, // 创建桌面图标
